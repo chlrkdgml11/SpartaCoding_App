2f7a2e02d8f7a0aadf2f288f9aaf77ffb0fe3d88
chlrkdgml11 <chlrkdgml11@naver.com>
2022-11-15 22:40:28 +0900

    SpartaCoind_App - Week 1-HW(2)

---
+++ Week 1/HW(2).js
@@ -0,0 +1,7 @@
+let fruit_list = ['사과','감','감','배','포도','포도','딸기','포도','감','수박','딸기']
+
+let podo_list = fruit_list.filter((value, index, array) => {
+    return value == "포도"
+})
+
+console.log(podo_list)
