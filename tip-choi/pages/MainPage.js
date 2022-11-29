6c68d051cd9deb316feb01b922d9eebd2ed3d542
chlrkdgml11 <chlrkdgml11@naver.com>
2022-11-29 16:25:09 +0900

    SpartaCoding_App - Week 3-3

--- tip-choi/pages/MainPage.js
+++ tip-choi/pages/MainPage.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 const main = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/main.png'
 import data from '../data.json';
 import Card from '../components/Card';
+import Loading from '../components/Loading';
 
 export default function MainPage() {
-  let tip = data.tip;
+  //useState 사용법
+        //[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
+  //setState는 state를 변경시킬때 사용해야하는 함수
+
+  //모두 다 useState가 선물해줌
+  //useState()안에 전달되는 값은 state 초기값
+  const [state,setState] = useState([])
+
+        //하단의 return 문이 실행되어 화면이 그려진다음 실행되는 useEffect 함수
+  //내부에서 data.json으로 부터 가져온 데이터를 state 상태에 담고 있음
+  const [ready,setReady] = useState(true)
+
+  // useEffect(()=>{
+  //   setState(data)
+  // },[])
+
+  useEffect(()=>{
+	   
+		//뒤의 1000 숫자는 1초를 뜻함
+    //1초 뒤에 실행되는 코드들이 담겨 있는 함수
+    setTimeout(()=>{
+        setState(data)
+        setReady(false)
+    },1000)
+ 
+    
+  },[])
+
+//   let tip = data.tip;
+  //data.json 데이터는 state에 담기므로 상태에서 꺼내옴
+  let tip = state.tip;
   let todayWeather = 10 + 17;
   let todayCondition = "흐림"
   //return 구문 밖에서는 슬래시 두개 방식으로 주석
-  return (
+  return ready ? <Loading/> : (
     /*
       return 구문 안에서는 {슬래시 + * 방식으로 주석
     */
     <ScrollView style={styles.container}>
-      <StatusBar style="dark" />
+      <StatusBar style='dark'></StatusBar>
       <Text style={styles.title}>나만의 꿀팁</Text>
       <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
       <Image style={styles.mainImage} source={{uri:main}}/>
@@ -29,7 +60,7 @@ export default function MainPage() {
       <View style={styles.cardContainer}>
         {/* 하나의 카드 영역을 나타내는 View */}
         { 
-          tip.map((content,i) => {
+          tip.map((content,i)=>{
             // return (<View style={styles.card} key={i}>
             //   <Image style={styles.cardImage} source={{uri:content.image}}/>
             //   <View style={styles.cardText}>
